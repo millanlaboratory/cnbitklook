8a461d772c4cc93268c606db1c7c154bac5b3532
Michele Tavella <tavella.michele@gmail.com>
2011-04-09 00:49:07 +0200

    Initial checkin for CcFile

---
+++ libcnbicore/src/libcnbicore/CcFile.cpp
@@ -0,0 +1,12 @@
+#ifndef CCFILE_CPP 
+#define CCFILE_CPP 
+
+#include "CcFile.hpp" 
+
+CcFile::CcFile(void) {
+}
+
+CcFile::~CcFile(void) {
+}
+
+#endif
---
+++ libcnbicore/src/libcnbicore/CcFile.hpp
@@ -0,0 +1,16 @@
+#ifndef CCFILE_HPP 
+#define CCFILE_HPP 
+
+class CcFile {
+	public:
+		CcFile(void);
+		virtual ~CcFile(void);
+	private:
+	protected:
+
+	public:
+	private:
+	protected:
+};
+
+#endif
