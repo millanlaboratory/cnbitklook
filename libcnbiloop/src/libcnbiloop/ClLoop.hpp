5c8ab7ece7c46fcb5bf3941dfe2f13a73f40a0f1
Michele Tavella <tavella.michele@gmail.com>
2011-04-10 10:07:35 +0200

    Added initial stub for ClLoop

---
+++ libcnbiloop/src/libcnbiloop/ClLoop.hpp
@@ -0,0 +1,34 @@
+/*
+    Copyright (C) 2011 Michele Tavella <tavella.michele@gmail.com>
+
+    This file is part of the libcnbiloop library
+
+    The libcnbiloop library is free software: you can redistribute it and/or
+    modify it under the terms of the version 3 of the GNU General Public
+    License as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CLLOOP_HPP 
+#define CLLOOP_HPP 
+
+class ClLoop {
+	public:
+		ClLoop(void);
+		virtual ~ClLoop(void);
+	private:
+	protected:
+
+	public:
+	private:
+	protected:
+};
+
+#endif
