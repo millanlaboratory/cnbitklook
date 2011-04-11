994a0006e289161fd00614a74a75167053608f23
Michele Tavella <tavella.michele@gmail.com>
2011-04-12 00:45:59 +0200

    Shitty Matlab calls are less messy now

--- libcnbiloop/src/libcnbiloop/ClMatlab.cpp
+++ libcnbiloop/src/libcnbiloop/ClMatlab.cpp
@@ -22,6 +22,83 @@
 #include <libcnbicore/CcBasic.hpp>
 #include "ClMatlab.hpp" 
 
+#define CLMATLAB_INCLUDE "\
+try;\
+	if(isempty(getenv('MTPATH_ROOT')));\
+		disp('MTPATH_ROOT unset, mtpath in /opt/mtpath');\
+		addpath('/opt/mtpath');\
+	end;\
+	status0 = mtpath_include('%s');\
+	if(status0 == false);\
+		status1 = mtpath_include('%s');\
+	end;\
+	if(status0 == false && status1 == false);\
+		disp('Cannot include at all');\
+		exit;\
+	end;\
+catch e;\
+	disp(['Exception: ' exception.message ]);\
+	disp(exception);\
+	disp(exception.stack);\
+	disp('Killing Matlab...');\
+	exit;\
+end;\n"
+
+#define CLMATLAB_ADDPATH "\
+try;\
+	status = exist('%s', 'dir');\
+	if(status == false);\
+		exit;\
+	else;\
+		addpath('%s');\
+	end;\
+catch e;\
+	disp(['Exception: ' exception.message ]);\
+	disp(exception);\
+	disp(exception.stack);\
+	disp('Killing Matlab...');\
+	exit;\
+end\n"
+
+#define CLMATLAB_CWD "\
+try;\
+	status = exist('%s', 'dir');\
+	cd('%s');\
+	if(status == false);\
+		exit;\
+	end;\
+catch e;\
+	disp(['Exception: ' exception.message ]);\
+	disp(exception);\
+	disp(exception.stack);\
+	disp('Killing Matlab...');\
+	exit;\
+end\n"
+
+#define CLMATLAB_LAUNCH	"\
+try;\
+	%s;\
+catch e;\
+	disp(['Exception: ' exception.message ]);\
+	disp(exception);\
+	disp(exception.stack);\
+	disp('Killing Matlab...');\
+	exit;\
+end\n"
+
+#define CLMATLAB_LAUNCHNDF "\
+try;\
+	while(1);\
+		%s('%s', '%s', '%s'%s);\
+	end;\
+catch e;\
+	disp(['Exception: ' exception.message ]);\
+	disp(exception);\
+	disp(exception.stack);\
+	disp('Killing Matlab...');\
+	exit;\
+end\n"
+
 ClMatlab::ClMatlab(const std::string& cmd, bool write, bool read) : 
 	CcProcess(cmd, write, read) {
 }
@@ -30,17 +107,13 @@ ClMatlab::~ClMatlab(void) {
 }
 
 void ClMatlab::Exec(void) { 
-	execlp(this->_cmd.c_str(), 
-		"-nodesktop", "-nojvm", "-nosplash",
-		"2>1", NULL);
+	execlp(this->_cmd.c_str(), "-nodesktop", "-nojvm", "-nosplash", "2>&1",
+			NULL);
 }
 
 void ClMatlab::AddPath(const std::string& path) {
 	char buffer[2048];
-	char format[2048] = "try; status = exist('%s', 'dir'); \
-						 if(status == false); exit; else; addpath('%s'); end; \
-						 catch e; exit; end\n";
-	sprintf(buffer, format, path.c_str(), path.c_str());
+	sprintf(buffer, CLMATLAB_ADDPATH, path.c_str(), path.c_str());
 
 	CcLogDebug(std::string("Adding path: ").append(buffer));
 	CcProcess::Write(buffer);
@@ -48,11 +121,7 @@ void ClMatlab::AddPath(const std::string& path) {
 
 void ClMatlab::Include(const std::string& path0, const std::string& path1) {
 	char buffer[2048];
-	char format[2048] = "try; status0 = mtpath_include('%s'); \
-						 if(status0 == false); status1 = mtpath_include('%s'); \
-						 end; if(status0 == false && status1 == false); exit; \
-						 end; catch e; exit; end\n";
-	sprintf(buffer, format, path0.c_str(), path1.c_str());
+	sprintf(buffer, CLMATLAB_INCLUDE, path0.c_str(), path1.c_str());
 	
 	CcLogDebug(std::string("Including path: ").append(buffer));
 	CcProcess::Write(buffer);
@@ -60,9 +129,7 @@ void ClMatlab::Include(const std::string& path0, const std::string& path1) {
 		
 void ClMatlab::ChangeDirectory(const std::string& path) {
 	char buffer[2048];
-	char format[2048] = "try; status = exist('%s', 'dir'); cd('%s'); \
-						 if(status == false); exit; end; catch e; exit; end\n";
-	sprintf(buffer, format, path.c_str(), path.c_str());
+	sprintf(buffer, CLMATLAB_CWD, path.c_str(), path.c_str());
 
 	CcLogDebug(std::string("Changing directory: ").append(buffer));
 	CcProcess::Write(buffer);
@@ -70,9 +137,7 @@ void ClMatlab::ChangeDirectory(const std::string& path) {
 
 void ClMatlab::Launch(const std::string function) {
 	char buffer[2048];
-	char format[2048] = "try; %s; catch e; exit; end\n";
-	
-	sprintf(buffer, format, function.c_str());
+	sprintf(buffer, CLMATLAB_LAUNCH, function.c_str());
 	CcLogDebug(std::string("Lanching: ").append(buffer));
 	CcProcess::Write(buffer);
 }
@@ -81,8 +146,6 @@ void ClMatlab::LaunchNDF(const std::string function, const std::string& pipename
 		const CcAddress addressD, const CcAddress addressC, 
 		const std::string& extra) {
 	char buffer[2048];
-	char format[2048] = "try; while(1);%s('%s', '%s', '%s'%s);end; \
-						 catch e; exit; end\n";
 
 	std::string optional("");
 	if(extra.empty() == false) {
@@ -90,7 +153,7 @@ void ClMatlab::LaunchNDF(const std::string function, const std::string& pipename
 		optional.append(extra);
 	}
 	
-	sprintf(buffer, format, function.c_str(), pipename.c_str(),
+	sprintf(buffer, CLMATLAB_LAUNCHNDF, function.c_str(), pipename.c_str(),
 			addressD.c_str(), addressC.c_str(), optional.c_str());
 
 	CcLogDebug(std::string("Lanching NDF: ").append(buffer));
