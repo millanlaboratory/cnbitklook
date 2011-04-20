36ceb148c83d22245b2131a513430818ad28955a
Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
2011-04-20 17:14:55 +0200

    Move libcnbiconfig to the common build system

--- doc/examples/cnbiconfig/cnbiconfig_online.cpp
+++ doc/examples/cnbiconfig/cnbiconfig_online.cpp
@@ -16,7 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "CCfgConfig.hpp"
+#include <libcnbiconfig/CCfgConfig.hpp>
+#include <libtobiic/ICMessage.hpp>
+#include <libtobiic/ICSerializerRapid.hpp>
 #include <iostream>
 #include <string>
 
@@ -41,24 +43,28 @@ int main(void) {
 			
 
 	/* We will now ask CCfgConfig to return the tasket 
-	 * identified by blockname and tasksetname
+	 * identified by blockname and tasksetname.
+	 * We also pass an ICMessage so that it gets configured depending on the
+	 * taskset.
 	 */
-	CCfgTaskset* offline = NULL;
+	CCfgTaskset* online = NULL;
+	ICMessage message;
 	try { 
-		offline = config.OfflineEx(blockname, tasksetname);
+		online = config.OnlineEx(blockname, tasksetname, &message);
 		cout << "Taskset assembled" << endl;
 	} catch(XMLException e) {
 		cout << "Exception: " << e << endl;
 		return 1;
 	}
+	message.Dump();
 
 	/* So far so good. Each tasket has several tasks, such as
 	 * right/left mi.  The number of tasks is not fixed.
 	 */
 	printf("Taskset: Name=%s, Description=%s, Tasks=%d\n", 
-			offline->name.c_str(), 
-			offline->description.c_str(), 
-			offline->Count());
+			online->name.c_str(), 
+			online->description.c_str(), 
+			online->Count());
 
 	/* Ok. Let's now imagine we want to configure an SMR fake feedback.
 	 * We need to know:
@@ -70,7 +76,7 @@ int main(void) {
 	 */
 
 	CCfgTasksetConstIt it;
-	for(it = offline->Begin(); it != offline->End(); it++)
+	for(it = online->Begin(); it != online->End(); it++)
 		(*it).second->Dump();
 
 	/* Cool. The complexity here is due to the nested nature of the XML
@@ -89,18 +95,19 @@ int main(void) {
 	/* I imagine I need to configure the CSmrCopilot. First thing, I need to get
 	 * the unique threshold we use for training.
 	 *
-	 * I heavily use CCfgXMLConfig here. The Ex methods raise exceptions.
+	 * I heavily use CCfgXMLConfig here. The Ex methods raise exceptions. 
+	 *
+	 * Here we try to get a unique-per block threshold. It will fail.
 	 */
 	float threshold = 0.00f;
 	try {
 		threshold = 
 			config.RootEx()->
-			GoEx("offline")->
+			GoEx("online")->
 			GoEx(blockname)->
 			QuickFloatEx("threshold");
 	} catch(XMLException e) {
 		cout << "Exception: " << e << endl;
-		return 1;
 	}
 	printf("Threshold is: %f\n", threshold);	
 	
@@ -111,14 +118,19 @@ int main(void) {
 	 * If the key is not there, it will segfault. Please check with the
 	 * iterators in real life;
 	 */
-	unsigned int* trials = new unsigned int(offline->Count());
-	HWTrigger* triggers = new HWTrigger(offline->Count());
-	GDFEvent* events = new GDFEvent(offline->Count());
-	for(it = offline->Begin(); it != offline->End(); it++) {
+	try {
+	float* thresholds = new float(online->Count());
+	unsigned int* trials = new unsigned int(online->Count());
+	HWTrigger* triggers = new HWTrigger(online->Count());
+	GDFEvent* events = new GDFEvent(online->Count());
+	for(it = online->Begin(); it != online->End(); it++) {
 		CCfgTask* task = it->second;
 		if(task->HasConfig("trials") == false)
 			cout << "Per-task configuration trials is missing" << endl;
+		if(task->HasConfig("threshold") == false)
+			cout << "Per-task configuration threshold is missing" << endl;
 		trials[task->id] = task->config["trials"].Int();
+		thresholds[task->id] = task->config["threshold"].Float();
 		triggers[task->id] = task->hwt;
 		events[task->id] = task->gdf;
 	}
@@ -126,10 +138,72 @@ int main(void) {
 	/* Good. We now have the trials, triggers and events, all stored in the 
 	 * 3 arrays. This means we can go online! Yuppi.
 	 */
-	for(unsigned int i = 0; i < offline->Count(); i++) { 
-		printf("ID=%d, Trials=%d, HWT=%d, GDF=%d\n", 
-				i, trials[i], triggers[i], events[i]);
+	for(unsigned int i = 0; i < online->Count(); i++) { 
+		printf("ID=%d, Trials=%d, HWT=%d, GDF=%d, Th=%f\n", 
+				i, trials[i], triggers[i], events[i], thresholds[i]);
+	}
+	} catch(XMLException e) {
+		cout << "Exception: " << e << endl;
 	}
 
+
+
+		/*
+		CCfgTaskset* offlineMI = NULL;
+		offlineMI = config.OfflineEx("smr", "mi_rhlh");
+		
+		ICMessage sender;
+		CCfgTaskset* onlineMI = config.OnlineEx("smr", "mi_rhlh", &sender);
+		CCfgTaskset* onlineErrP = config.OnlineEx("smrerrp", "errp", &sender);
+
+
+		cout << ">>>> MI classifier: " << onlineMI->classifier << endl;
+		cout << ">>>> ErrP classifier: " << onlineErrP->classifier << endl;
+
+		onlineMI->GetEx("mi_hand_right")->Dump();
+		onlineMI->GetEx("mi_hand_left")->Dump();
+		cout << "Integration: " << config.RootEx()->GoEx("online")->
+			GoEx("smr")->GoEx("integration")->GetEx().Float() << endl;
+		onlineErrP->GetEx("errp_true")->Dump();
+		cout << "Threshold: " << 
+			onlineMI->GetEx("mi_hand_left")->config["threshold"].Float() << endl;
+		
+		cout << "By id: " << onlineMI->Count() << endl;
+		CCfgTask* t = NULL;
+		t = onlineMI->GetEx(1);
+		t->Dump();
+		t = onlineMI->GetEx(0);
+		t->Dump();
+
+		cout << "Go: device is: " << config.RootEx()->GoEx("options")->
+			GoEx("fes")->GoEx("dev")->GetEx().String() << endl;
+		cout << "Quick: device is: " << 
+			config.RootEx()->QuickEx("options/fes/dev")->GetEx().String() << endl;
+		cout << "QuickString: device is: " << 
+			config.RootEx()->QuickStringEx("options/fes/dev") << endl;
+		cout << "GDF off: " << 
+			config.RootEx()->QuickGDFIntEx("protocol/gdfevents/off") << endl;
+		cout << "GDF cfeedback: " << 
+			config.RootEx()->QuickGDFIntEx("protocol/gdfevents/cfeedback") << endl;
+		cout << "GDF cfeedbackoff: " << 
+			config.RootEx()->QuickGDFIntEx("protocol/gdfevents/cfeedbackoff") << endl;
+
+		
+		std::string buffer;
+		ICSerializerRapid serializer(&sender, true);
+		serializer.Serialize(&buffer);
+		
+		ICMessage receiver;
+		ICSerializerRapid deserializer(&receiver, true);
+		deserializer.Deserialize(&buffer);
+		
+		sender.Dump();	
+		receiver.Dump();
+
+		cout << "Subject is: " << 
+			config.RootEx()->GoEx("subject")->GoEx("id")->GetEx().String() << endl;
+		cout << "Error is: " << endl; 
+		cout << config.RootEx()->GoEx("subject")->GoEx("name")->GetEx().String() << endl;
+		*/
 	return 0;
 }
--- doc/examples/cnbiconfig/smarttype.cpp
+++ doc/examples/cnbiconfig/smarttype.cpp
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "XMLType.hpp"
+#include <libcnbiconfig/XMLType.hpp>
 #include <string>
 #include <iostream>
 
