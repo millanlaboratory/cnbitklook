0a57efe26e8367756e90cde83576892d8168696d
Michele Tavella <tavella.michele@gmail.com>
2011-05-13 10:32:49 +0200

    Updated GPL

--- matndf/ndf_tobi_sendc.m
+++ matndf/ndf_tobi_sendc.m
@@ -1,20 +1,18 @@
-%   Copyright (C) 2011 Michele Tavella <tavella.michele@gmail.com>
+% Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+% Michele Tavella <michele.tavella@epfl.ch>
 %
-%   This file is part of matndf
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
 %
-%   The libndf library is free software: you can redistribute it and/or
-%   modify it under the terms of the version 3 of the GNU General Public
-%   License as published by the Free Software Foundation.
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
 %
-%   This program is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%   GNU General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License
-%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%
-%   function bytes = ndf_tobi_sendc(tobi, frameidx)
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 function bytes = ndf_tobi_sendc(tobi, frameidx)
 
 icmessage_absolutetic(tobi.iC.message);
--- matndf/printf.m
+++ matndf/printf.m
@@ -1,8 +1,9 @@
-% Copyright (C) 2007 Michele Tavella <michele@liralab.it>
+% Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+% Michele Tavella <michele.tavella@epfl.ch>
 %
-% This program is free software; you can redistribute it and/or modify
+% This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2 of the License, or
+% the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
 % This program is distributed in the hope that it will be useful,
@@ -11,8 +12,8 @@
 % GNU General Public License for more details.
 %
 % You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function printf(varargin)
 
 fprintf(1, varargin{:});
