3a3e1633ab525a7f4925082919ba6c364888459f
Michele Tavella <tavella.michele@gmail.com>
2011-05-23 08:46:19 +0200

    Now solving TOBICOREMAT_ROOT

--- mex/cnbitkmat/cnbitkmat_init.m
+++ mex/cnbitkmat/cnbitkmat_init.m
@@ -1,6 +1,14 @@
 [~, basepath] = mtpath_basename(which('cnbitkmat_init.m'));
+	
+disp(['[cnbitkmat_init] $CNBITKMAT_ROOT: ' getenv('CNBITKMAT_ROOT')]);
+disp(['[cnbitkmat_init] $TOBICOREMAT_ROOT: ' getenv('TOBICOREMAT_ROOT')]);
 
 mtpath_include([basepath '/matndf/']);
 mtpath_include([basepath '/mexcnbiloop']);
 mtpath_include([basepath '/mexcnbiconfig']);
 mtpath_include([basepath '/mextransport']);
+
+if(isempty(getenv('TOBICOREMAT_ROOT')))
+    setenv('TOBICOREMAT_ROOT', '/opt/tobicoremat/');
+end
+mtpath_include('$TOBICOREMAT_ROOT');
