ec6adeb548410ec3a55132cee515eea5db8bb5e9
Michele Tavella <tavella.michele@gmail.com>
2011-05-23 06:54:48 +0200

    Some changes

--- mex/cnbitkmat/cnbitkmat_init.m
+++ mex/cnbitkmat/cnbitkmat_init.m
@@ -4,6 +4,3 @@ mtpath_include([basepath '/matndf/']);
 mtpath_include([basepath '/mexcnbiloop']);
 mtpath_include([basepath '/mexcnbiconfig']);
 mtpath_include([basepath '/mextransport']);
-mtpath_include([basepath '/mextobicore']);
-mtpath_include([basepath '/mextobiic']);
-mtpath_include([basepath '/mextobiid']);
