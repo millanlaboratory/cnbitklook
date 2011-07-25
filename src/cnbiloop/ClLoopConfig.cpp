b511c4aa065af6ee006ca1bb1f8a0db35753d508
Michele Tavella <tavella.michele@gmail.com>
2011-07-25 13:22:38 +0200

    Added ClLoopConfig stub

---
+++ src/cnbiloop/ClLoopConfig.cpp
@@ -0,0 +1,12 @@
+#ifndef CLLOOPCONFIG_CPP 
+#define CLLOOPCONFIG_CPP 
+
+#include "ClLoopConfig.hpp" 
+
+ClLoopConfig::ClLoopConfig(void) {
+}
+
+ClLoopConfig::~ClLoopConfig(void) {
+}
+
+#endif
---
+++ src/cnbiloop/ClLoopConfig.hpp
@@ -0,0 +1,16 @@
+#ifndef CLLOOPCONFIG_HPP 
+#define CLLOOPCONFIG_HPP 
+
+class ClLoopConfig {
+	public:
+		ClLoopConfig(void);
+		virtual ~ClLoopConfig(void);
+	private:
+	protected:
+
+	public:
+	private:
+	protected:
+};
+
+#endif
