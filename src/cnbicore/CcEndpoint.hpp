ddd79587277d3ef4a943e943e298eb9f4fcc326c
Serafeim Perdikis <serafeim.perdikis@epfl.ch>
2016-07-21 21:16:59 +0200

    Fixed bug in header ifndef of CcEndPoint.hpp This was causing problems when including CcEndPoint

--- src/cnbicore/CcEndpoint.hpp
+++ src/cnbicore/CcEndpoint.hpp
@@ -16,8 +16,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CCHOST_HPP
-#define CCHOST_HPP
+#ifndef CCENDPOINT_HPP
+#define CCENDPOINT_HPP
 
 #include "CcNetworkTypes.hpp"
 #include "CcObject.hpp"
